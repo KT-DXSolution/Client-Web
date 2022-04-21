e4022b3dc409d72f77c004187b4266f582971cec
jigyeong <wlrud0126@gmail.com>
2022-04-21 10:13:03 +0900

    등록 버튼 추가

--- src/components/ObjectList.js
+++ src/components/ObjectList.js
@@ -1,7 +1,6 @@
 // Chakra imports
 import {
   Flex,
-  Icon,
   Table,
   Tbody,
   Text,
@@ -9,12 +8,12 @@ import {
   Thead,
   Tr,
   useColorModeValue,
+  Button
 } from "@chakra-ui/react";
 import Card from "components/Card/Card.js";
 import CardHeader from "components/Card/CardHeader.js";
 import DashboardTableRow from "components/Tables/DashboardTableRow";
 import React from "react";
-import { IoCheckmarkDoneCircleSharp } from "react-icons/io5";
 
 const ObjectList = ({ title, amount, captions, data }) => {
   const textColor = useColorModeValue("gray.700", "white");
@@ -22,25 +21,18 @@ const ObjectList = ({ title, amount, captions, data }) => {
   return (
     <Card p='16px' overflowX={{ sm: "scroll", xl: "hidden" }}>
       <CardHeader p='12px 0px 28px 0px'>
-        <Flex direction='column'>
-          <Text fontSize='lg' color={textColor} fontWeight='bold' pb='.5rem'>
+        <Flex justify='space-between' align='center' mb='1rem' w='100%'>
+          <Text fontSize='lg' color={textColor} fontWeight='bold' pb='.5rem' style={{padding:"0"}}>
             {title}
           </Text>
-          <Flex align='center'>
-            <Icon
-              as={IoCheckmarkDoneCircleSharp}
-              color='teal.300'
-              w={4}
-              h={4}
-              pe='3px'
-            />
-            <Text fontSize='sm' color='gray.400' fontWeight='normal'>
-              <Text fontWeight='bold' as='span'>
-                {amount} done
-              </Text>{" "}
-              this month.
-            </Text>
-          </Flex>
+          <Button
+            colorScheme='teal'
+            variant='solid'
+            fontSize='s'
+            p='8px 32px'
+            style={{cursor:"pointer"}}>
+            등록
+          </Button>
         </Flex>
       </CardHeader>
       <Table variant='simple' color={textColor}>
