a91b43e0609ae40032d7489941c4b37c66e9858a
jigyeong <wlrud0126@gmail.com>
2022-05-18 18:05:37 +0900

    ADD : serviceworker

---
+++ src/serviceworker.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyADWtr1syccBY5AN0WUWA39Jl9V0VvDKUQ",
+  authDomain: "closing-sale.firebaseapp.com",
+  projectId: "closing-sale",
+  storageBucket: "closing-sale.appspot.com",
+  messagingSenderId: "1001500685848",
+  appId: "1:1001500685848:web:4a4b57a74c5bf0cb8d356d",
+  measurementId: "G-H12V3N2XDH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
